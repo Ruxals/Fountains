568782cbd3dfa2154ea304945bda308e9de36e54
Ruxals <marcosilvaiscool159@gmail.com>
2025-09-25 15:36:28 -0500

    Some obvious tweaks have and still need to be made.

--- Fountains/Program.cs
+++ Fountains/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System;
 public class Grid<T>
 {
     public int Row { get; set; }
@@ -23,7 +23,7 @@ public class Player
         get => _prow;
         set
         {
-            if (_prow < compareGrid.Row || _prow > compareGrid.Row) { Console.WriteLine("Out of bounds."); }
+            if (_prow < 0 || _prow > compareGrid.Row) { Console.WriteLine("Out of bounds."); }
             _prow = value;
         }
     }
@@ -32,7 +32,7 @@ public class Player
         get => _pcolumn;
         set
         {
-            if (_pcolumn < compareGrid.Column || _pcolumn > compareGrid.Column) { Console.WriteLine("Out of bounds."); }
+            if (_pcolumn < 0 || _pcolumn > compareGrid.Column) { Console.WriteLine("Out of bounds."); }
             _prow = value;
         }
     }
@@ -46,6 +46,7 @@ public class Fountain
     public int Fcol { get => _fcol; set { } }
 
     public bool isFound = false;
+    public bool isEnabled = false;
 }
 
 public class Game
@@ -54,5 +55,69 @@ public class Game
     public Player player = new Player();
     public Fountain fountain = new Fountain();
 
-    // hi uhh Im testing... yup
+    public void Setting(int distance)
+    {
+        if (distance == 0) { fountain.isFound = true; Console.WriteLine("You have found the fountain!"); }
+        if (distance == 1) { Console.WriteLine("Within the caverns you hear droplets from a near. You must be close."); }
+        if (distance == 2) { Console.WriteLine("It seems you are in the deep cavern with little sheds of lights. Continue onward."); }
+        if (distance == 3) { Console.WriteLine("It seems you are in the deeper cavern with no sheds of light. It's a midnight zone here."); }
+        if (distance == 4) { Console.WriteLine("You lie in the deep abyss, this is no place for you."); }
+        Console.WriteLine("You hear the cavern's emanations. Continue Onward");
+    }
+    public void Displacement()
+    {
+        int rowDif = Math.Abs(player.Prow - fountain.Frow);
+        int columnDif = Math.Abs(player.PColumn - fountain.Fcol);
+        int totalDif = rowDif + columnDif;
+        Setting(totalDif);
+    }
+    public void Status(string direction)
+    {
+        switch (direction)
+        {
+            case "move east":
+                player.PColumn++;
+                Displacement();
+                break;
+            case "move west":
+                player.PColumn--;
+                Displacement();
+                break;
+            case "move north":
+                player.Prow++;
+                Displacement();
+                break;
+            case "move south":
+                player.Prow--;
+                Displacement();
+                break;
+            default:
+                Console.WriteLine("Make sure the input is lowercased.");
+                break;
+        }
+    }
+
+
+
+    public void Run()
+    {
+        string direction;
+        do
+        {
+            Console.WriteLine("Options are to move in each compass direction i.e. type, move east, to move 1 space to the right");
+            Console.WriteLine($"(Row = {player.Prow}, Column = {player.PColumn})");
+            Console.WriteLine(("What would you like to do: "));
+            direction = Console.ReadLine();
+            Status(direction);
+        } while (fountain.isFound == false && fountain.isEnabled == false);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Game game = new Game();
+        game.Run();
+    }
 }
